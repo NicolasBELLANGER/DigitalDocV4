c79f55e3182ee56cc9241c7ec61f8d36c022dec1
NicolasBELLANGER <nicobell93220@gmail.com>
2024-05-17 16:59:10 +0200

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(ManagerRegistry $doctrine): Response
     {
+        $user = $this->getUser();
+        if (!$user instanceof User || (!$user->hasRole('Administrateur') && !$user->hasRole('Editeur') && !$user->hasRole('Editeur'))) {
+            return $this->redirectToRoute('app_login');
+        }
+
         $articles = $doctrine->getRepository(Article::class)->findAll();
         return $this->render('home/index.html.twig', [
             'articles' => $articles,
